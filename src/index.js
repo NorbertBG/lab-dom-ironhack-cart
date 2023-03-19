eecd0c09d7141e0072b95bfb0a8cdb09320a79e4
Norbert Busom <norbertbg@outlook.com>
2023-03-19 19:54:41 +0100

    lab solved, no bonus

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span').textContent;
+  const quantity = product.querySelector('.quantity input').value;
 
-  //... your code goes here
+  const totalAmount = Number(price) * quantity;
+
+  product.querySelector('.subtotal span').innerHTML = totalAmount;
+  return totalAmount;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let totalAmount = 0;
+  const products = document.querySelectorAll('.product');
+  products.forEach( product => totalAmount += updateSubtotal(product))
+
 
   // ITERATION 3
-  //... your code goes here
+  document.querySelector("#total-value span").innerHTML = totalAmount;
 }
 
 // ITERATION 4
@@ -26,6 +33,7 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+  
 }
 
 // ITERATION 5
